43060fd617397a16a98f3ec3569b71f199f81579
anill1412 <anilkumarsipurusetty@gmail.com>
2024-10-23 18:06:47 +0530

    Changed the Guest User Details

--- MERN-Educational-Management-System-main/frontend/src/pages/LoginPage.js
+++ MERN-Educational-Management-System-main/frontend/src/pages/LoginPage.js
@@ -74,23 +74,23 @@ const LoginPage = ({ role }) => {
     };
 
     const guestModeHandler = () => {
-        const password = "zxc"
+        const password = "1234"
 
         if (role === "Admin") {
-            const email = "yogendra@12"
+            const email = "anil@gmail.com"
             const fields = { email, password }
             setGuestLoader(true)
             dispatch(loginUser(fields, role))
         }
         else if (role === "Student") {
-            const rollNum = "1"
-            const studentName = "Dipesh Awasthi"
+            const rollNum = "4"
+            const studentName = "anil"
             const fields = { rollNum, studentName, password }
             setGuestLoader(true)
             dispatch(loginUser(fields, role))
         }
         else if (role === "Teacher") {
-            const email = "tony@12"
+            const email = "anil"
             const fields = { email, password }
             setGuestLoader(true)
             dispatch(loginUser(fields, role))
